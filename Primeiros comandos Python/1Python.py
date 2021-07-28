3936878d276c30c560637c3609160c3063a19956
Taiane <taianens1d@gmail.com>
2021-07-27 21:55:44 -0300

    Primeiros comando usando o Python como linguagem

---
+++ Primeiros comandos Python/1Python.py
@@ -0,0 +1,7 @@
+nome = input ('Como você chama ?')
+print ('Seja bem vinde',nome)
+idade = input('Me diga sua idade')
+print ('Que legal,', idade, 'é uma idade otima')
+cidade = input ('Qual cidade você mora?')
+print (cidade,'é um cidade incrivel, sempre quis visitar essa cidade', nome)
+
