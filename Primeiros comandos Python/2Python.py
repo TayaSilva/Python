29297a5d4330a3959398ee6d359f49aca5081b01
Taiane Silva <taianens1d@gmail.com>
2022-05-27 16:07:48 -0300

    Add files via upload

---
+++ Primeiros comandos Python/2Python.py
@@ -0,0 +1,8 @@
+n1 = int (input('Digite um numero')) # igual(=) é receber!
+n2 = int (input('Digite outro numero'))
+n3 = int(input ('Digite mais um numero'))
+res = n1+n2+n3
+#print('A soma entre', n1,n2, 'é {}!'.format(res))
+print('A soma do numero {}, {} e {} é igual a {}'.format(n1, n2, n3, res))
+
+#int, float, bool (booleanos e str (string)
