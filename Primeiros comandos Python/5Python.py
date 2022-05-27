819942d79453cb03c8415ef28691353ce0141d91
Taiane Silva <taianens1d@gmail.com>
2022-05-27 17:25:04 -0300

    Add files via upload

---
+++ Primeiros comandos Python/5Python.py
@@ -0,0 +1,6 @@
+print('Olá amigo, aqui vamos descobrir o numero antes e depois do que você digitar')
+n=int(input('Digite um numero'))
+a = n-1
+s = n+1
+
+print('Segundos nossos calculos, o antecessor do valor {} é {} e o sucessor é {}'.format(n,a,s))
