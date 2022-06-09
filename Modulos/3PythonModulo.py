3c1d3e9d870916bc7e4d9bd7feae1d22e2bd0e0c
Taiane Silva <taianens1d@gmail.com>
2022-06-09 12:29:50 -0300

    Add files via upload

---
+++ Modulos/3PythonModulo.py
@@ -0,0 +1,10 @@
+'''from math import trunc
+num = float(input("Digite um numero: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(num, trunc(num)))'''
+
+num = float(input("Digite um numero: "))
+print("O valor digitado foi {} e a sua porção inteira é {}".format(num, int(num)))
+
+#Fazer aparecer apenas os numeros inteiros
+
+
