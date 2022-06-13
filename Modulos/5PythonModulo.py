9a65bebc044c299ba59ead70ef00edc8bb4d6799
Taiane Silva <taianens1d@gmail.com>
2022-06-13 14:36:25 -0300

    Add files via upload

---
+++ Modulos/5PythonModulo.py
@@ -0,0 +1,17 @@
+import random
+
+
+'''Um professor quer sortear um dos seus quatro alunos para apagar o quadro.
+Faça um programa que ajude ele, lendo o nome dos alunos e escrevendo na tela o
+nome do escolhido.'''
+
+a1 = input("Digite o nome do primeiro aluno: ")
+a2 = input("Digite o nome do segundo aluno: ")
+a3 = input("Digite o nome do terceiro aluno: ")
+a4 = input("Digite o nome do quarto aluno: ")
+
+lista = [a1,a2,a3,a4]
+
+escolhido = random.choice(lista)
+
+print("O aluno escolhido no sorteio foi {}".format(escolhido))
