a415821308c8fa343cb34d54d55ae6c33440f896
Taiane Silva <taianens1d@gmail.com>
2022-06-13 16:08:38 -0300

    Add files via upload

---
+++ Cadeia de texto/2PythonString.py
@@ -0,0 +1,30 @@
+'''Crie um programa que leia o nome completo de uma pessoa e mostre:
+
+– O nome com todas as letras maiúsculas e minúsculas.
+
+– Quantas letras ao todo (sem considerar espaços).
+
+– Quantas letras tem o primeiro nome'''
+
+nome = str(input("Digite seu nome completo: ")).strip()
+print('Analisando seu nome ...')
+
+print("Seu nome em maiusculo é {}".format(nome.upper()))
+print("Seu nome em minusculo é {}".format(nome.lower()))
+print("Seu nome ao todo tem {} letras".format(len(nome) - nome.count(' ')))
+'''Fez a contagem de letras e subtraiu os espaços'''
+
+#print("Seu primeiro nome tem {} letras".format(nome.find(' ')))
+separa = nome.split()
+
+print("Seu primeiro nome é {} e ele tem {} letras".format(separa[0], len(separa[0])))
+
+
+
+
+
+
+
+
+
+
