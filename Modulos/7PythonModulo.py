0888d600db03b4bd5544eec459351736de10dfcd
Taiane Silva <taianens1d@gmail.com>
2022-06-13 15:04:15 -0300

    Add files via upload

---
+++ Modulos/7PythonModulo.py
@@ -0,0 +1,12 @@
+import pygame
+
+# Inicializando o mixer PyGame
+pygame.mixer.init()
+
+# Iniciando o Pygame
+pygame.init()
+
+pygame.mixer.music.load('msc.mp3')
+pygame.mixer.music.play(loops=0, start=0.0,)
+pygame.event.wait()
+
