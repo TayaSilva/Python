f14ac15bccf26f1186f0535f9a68013565e18bb5
Taiane Silva <taianens1d@gmail.com>
2022-06-20 18:49:19 -0300

    Add files via upload

---
+++ Condição Aninhada/3PythonCondiçãoA.py
@@ -0,0 +1,9 @@
+num1 = float(input("Digite o primeiro numero: "))
+num2 = float(input("Digite o segundo numero: "))
+
+if num1 < num2:
+    print("O SEGUNDO numero é MAIOR!!!")
+elif num1 > num2:
+    print("O PRIMEIRO numero é MAIOR!!!")
+elif num1 == num2:
+    print("Os numeros são IGUAIS!!!")
