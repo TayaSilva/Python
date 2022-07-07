c86c38a9f9adebd0e24dade3e8f16016bb0ab635
Taiane Silva <taianens1d@gmail.com>
2022-07-07 19:38:21 -0300

    Add files via upload

---
+++ While/8PythonWhile.py
@@ -0,0 +1,17 @@
+
+num = soma = cont = 0
+while num != 999:
+    num = int(input("Digite um numero ou 999 para parar o programa:  "))
+    soma += num
+    cont += 1
+
+
+print("você digitou {} numero e a soma entre eles é {}".format(cont -1 ,soma -999))
+print("Programa encerrado")
+
+
+
+
+
+
+
